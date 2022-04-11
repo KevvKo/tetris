b88df7f2e334d35ecdf80377d5b317e677c970ef
KevvKo <kklein915@gmail.com>
2022-04-11 17:53:51 +0200

    add pieces

---
+++ pieces.h
@@ -0,0 +1,13 @@
+#ifndef _PIECES_
+#define _PIECES_
+
+class Pieces {
+
+	public: 
+
+		int GetBlockType ( int pPiece, int pRotation, int pX, intpY);
+		int GetXInitialPosition ( int pPiece, int pRotation );
+		int GetYnitialPosition ( int pPiece, int pRotation );
+}
+
+#endif 
