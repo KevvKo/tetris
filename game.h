4be42c1cd60c2e80b0c4f924e5e1a9904e1d2c2c
KevvKo <kklein915@gmail.com>
2022-04-16 19:31:08 +0200

    changes for header file

--- game.h
+++ game.h
@@ -3,3 +3,36 @@
 
 #include "board.h"
 #include "pieces.h"
+#include <time.h>
+
+#define WAIT_TIME 700 // number of milliseconds that remain before a piece going one block down
+
+class Game {
+
+	public:
+
+		Game 					(board *pBoard, Piece *pPieces, IO *pIO, int pScreenHeight);
+
+		void drawScene			();
+		void createNewPiece		();
+
+		int mPosX, mPosY;				// position of the pieces that is falling down
+		int mPiece, mRotation;			// kind of rotation that is falling down
+
+	private:
+
+		int mScreenHeight; 				// screen height in pixels		
+		int mNextPosX, mNextPosY;		// position of the next piece
+		int mNextPiece, mNextRotation;	// kind and rotation of the next piece
+
+		Board *mBoard;
+		Pieces *mPieces;
+		IO *mIO;
+
+		int getRand				(int pA, int pB);
+		void initGame			();
+		void drawPiece			(int pX, int pY, int pPiece, int pRotation);
+		void drawBoard			();
+
+}
+#endif
